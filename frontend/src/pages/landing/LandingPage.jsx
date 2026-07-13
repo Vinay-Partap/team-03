4741e17e63eb41fa0f8a75b32ce51f71ef4a7817
Vinay-Partap <bhardwajvinay400@gmail.com>
2026-07-13 22:33:27 +0530

    feat(frontend): setup routing and layouts

---
+++ frontend/src/pages/landing/LandingPage.jsx
@@ -0,0 +1,7 @@
+export default function LandingPage() {
+    return (
+      <h1 className="text-5xl font-bold text-center mt-20">
+        Landing Page
+      </h1>
+    );
+  }
---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,47 @@
+import { Routes, Route } from "react-router-dom";
+
+import PublicLayout from "../layouts/PublicLayout";
+import AuthLayout from "../layouts/AuthLayout";
+import CitizenLayout from "../layouts/CitizenLayout";
+import GovernmentLayout from "../layouts/GovernmentLayout";
+
+import LandingPage from "../pages/landing/LandingPage";
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+import ForgotPassword from "../pages/auth/ForgotPassword";
+import ResetPassword from "../pages/auth/ResetPassword";
+
+import Dashboard from "../pages/citizen/Dashboard";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+
+      {/* Public */}
+
+      <Route element={<PublicLayout />}>
+        <Route path="/" element={<LandingPage />} />
+      </Route>
+
+      {/* Auth */}
+
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Route>
+
+      {/* Citizen */}
+
+      <Route element={<CitizenLayout />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
+
+      {/* Government */}
+
+      <Route element={<GovernmentLayout />}></Route>
+
+    </Routes>
+  );
+}
